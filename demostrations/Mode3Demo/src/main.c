3271122cf8ff8ce796b69ff59fd3687bbe57d51e
linobigatti <linobigatti2015@gmail.com>
2019-02-05 13:53:47 -0300

    Change the mode 3 demo.

--- demostrations/Mode3Demo/src/main.c
+++ demostrations/Mode3Demo/src/main.c
@@ -16,14 +16,14 @@ int main(void) {
 	m3_frame(100, 60, 40, 40, m3_RGB_color(0, 0, 0));
 	m3_rectangle(101, 61, 39, 39, m3_RGB_color(31, 31, 31));
 	
+	m3_circle(0, 0, 30, m3_RGB_color(31, 0, 0));
+	m3_circle(0, 120, 30, m3_RGB_color(31, 0, 0));
+	
 	m3_line(0, 0, 240, 160, m3_RGB_color(31, 0, 31));
-	m3_line(0, 160, 240, 0, m3_RGB_color(31, 0, 31));
+	m3_line(240, 0, 0, 160, m3_RGB_color(31, 0, 31));
 	
 	m3_line(0, 80, 240, 80, m3_RGB_color(0, 31, 31));
 	m3_line(120, 0, 120, 160, m3_RGB_color(0, 31, 31));
-	
-	m3_circle(0, 0, 30, m3_RGB_color(31, 0, 0));
-	m3_circle(0, 160, 30, m3_RGB_color(31, 0, 0));
 	
 	while(1) {
 		Vsync();
