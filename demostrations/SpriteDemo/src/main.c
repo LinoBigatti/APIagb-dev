e5d40cc0cfc3598398aca47ed0bb3a68b9bd0638
linobigatti <linobigatti2015@gmail.com>
2019-02-09 22:55:01 -0300

    Add movement to SpriteDemo.

--- demostrations/SpriteDemo/src/main.c
+++ demostrations/SpriteDemo/src/main.c
@@ -19,12 +19,41 @@ int main(void) {
 	
 	obj_attributes *mario = &obj_reg_memory[0];
 	
+	int x = 0, y = 0;
+	
 	mario->attr0 = attr0_build(attr0_Y(0), attr0_regular, attr0_normal, 0, attr0_4bpp, attr0_square);
 	mario->attr1 = attr1_regular_build(attr1_X(0), 0, 0, attr1_size(1));
 	mario->attr2 = attr2_build(attr2_base_tile(0), attr2_priority(0), attr2_palbank(0));
 	
 	while(1) {
 		Vsync();
+		
+		key_update();
+		
+		if(key_is_pressed(BUTTON_RIGHT)) {
+			if((x + 13) < 240) {
+				x += 1;
+			}
+		}
+		if(key_is_pressed(BUTTON_LEFT)) {
+			if(x > 0) {
+				x -= 1;
+			}
+		}
+		if(key_is_pressed(BUTTON_DOWN)) {
+			if((y + 16) < 160) {
+				y += 1;
+			}
+		}
+		if(key_is_pressed(BUTTON_UP)) {
+			if(y > 0) {
+				y -= 1;
+			}
+		}
+		
+		mario->attr0 = attr0_build(attr0_Y(y), attr0_regular, attr0_normal, 0, attr0_4bpp, attr0_square);
+		mario->attr1 = attr1_regular_build(attr1_X(x), 0, 0, attr1_size(1));
+		mario->attr2 = attr2_build(attr2_base_tile(0), attr2_priority(0), attr2_palbank(0));
 	}
 	
 	return 0;
