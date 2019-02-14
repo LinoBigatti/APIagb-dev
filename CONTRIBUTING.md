6136bd3b5e08e858ddbbc2ed3aad64601de3fa1a
linobigatti <linobigatti2015@gmail.com>
2019-02-14 10:43:05 -0300

    Fix Mode5Demo.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -46,4 +46,4 @@ Brackets must be ahead of the function, like this: `u32 foo(u64 baz) {`.
 
 Once all of this point are done, the repo will be cloned into [the stable repository](https://github.com/linobigatti/APIagb).
 
-* Check, fix, and upload all the demos.
+* Check, fix, and upload all the demos. ✓
--- demostrations/Mode5Demo/src/main.c
+++ demostrations/Mode5Demo/src/main.c
@@ -23,7 +23,33 @@ int main(void) {
 	m5_fill(m5_RGB_color(10, 10, 10));
 	
 	m5_rectangle(51, 35, 59, 59, m5_RGB_color(0, 0, 31));
+	m5_frame(50, 34, 60, 60, m5_RGB_color(31, 31, 31));
 	
+	m5_circle(80, 64, 30, m5_RGB_color(0, 31, 0));
+	
+	for(ii = 0; ii < 9; ii++) {
+		jj_ = 3.4444444 * ii;
+		jj = Round(jj_);
+		m5_line(0 + 20 * ii, 0, 160 - 20 * ii, 128, m5_RGB_color(0, jj, jj));
+	}
+	
+	m5_line(0, 64, 160, 64, m5_RGB_color(31, 0, 0));
+	
+	m5_flip();
+	
+	for(ii = 0; ii < 16; ii++) {
+		jj_ = 2.06666666667 * ii;
+		jj = Round(jj_);
+		m5_line(0, 0 + 8 * ii, 160, 0 + 8 * ii, m5_RGB_color(jj, 0, jj));
+	}
+	
+	for(ii = 0; ii < 16; ii++) {
+		jj_ = 2.06666666667 * ii;
+		jj = Round(jj_);
+		m5_line(0 + 10 * ii, 0, 0 + 10 * ii, 128, m5_RGB_color(0, jj, jj));
+	}
+	
+	m5_flip();
 	
 	while(1) {
 		Vsync();
