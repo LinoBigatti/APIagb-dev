6466971f6293958faefc5d40e0d2f78598ab89a3
Lino Bigatti <linobigatti2015@gmail.com>
2019-11-23 19:27:13 -0300

    Solved LZ77 graphics problem.

--- demostrations/InterruptDemo/include/bowser.h
+++ demostrations/InterruptDemo/include/bowser.h
@@ -5,10 +5,10 @@
 //
 //	bowser, 64x64@8, 
 //	+ palette 256 entries, not compressed
-//	+ 64 tiles Metatiled by 8x8 not compressed
-//	Total size: 512 + 4096 = 4608
+//	+ 64 tiles lz77 compressed
+//	Total size: 512 + 1012 = 1524
 //
-//	Time-stamp: 2019-02-13, 16:49:20
+//	Time-stamp: 2019-11-23, 19:25:13
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -17,8 +17,8 @@
 #ifndef GRIT_BOWSER_H
 #define GRIT_BOWSER_H
 
-#define bowserTilesLen 4096
-extern const unsigned short bowserTiles[2048];
+#define bowserTilesLen 1012
+extern const unsigned short bowserTiles[506];
 
 #define bowserPalLen 512
 extern const unsigned short bowserPal[256];
